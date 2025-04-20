e6871adcbdb8c77a7c80c392eb9c3ed515c36999
Daniyanaj <89309719+Daniyanaj@users.noreply.github.com>
2025-04-21 01:56:40 +0400

    Update train_inference.sh

--- vMixer/train_inference.sh
+++ vMixer/train_inference.sh
@@ -21,6 +21,15 @@ then
 	CUDA_VISIBLE_DEVICES=${cuda} vmixer_train 3d_fullres vmixerTrainerV2_${name} ${task} 0
 fi
 
+if ${predict}
+then
+
+
+	cd /home/daniya.kareem/vmixer/vmixer/DATASET/vmixer_raw/vmixer_raw_data/Task002_Synapse/
+	CUDA_VISIBLE_DEVICES=${cuda} vmixer_predict -i imagesTs -o inferTs/${name} -m 3d_fullres -t ${task} -f 0 -chk model_best -tr vmixerTrainerV2_${name}
+	python inference_synapse.py ${name}
+fi
+
 
 
 
